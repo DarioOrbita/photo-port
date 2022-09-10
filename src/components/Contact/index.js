7d3d6408c14145a78079df538663642a69aa35e4
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-09 22:45:09 -0500

    add dynamic form submission

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,22 @@
-import react from "react";
+import react, {useState} from "react";
 
 function ContactForm() {
+    const {formState, setFormState} = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+      }
+
   return (
     <section>
       <h1>Contact me</h1>
@@ -8,21 +24,21 @@ function ContactForm() {
        [// name input]
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
         </div>
         [// email input]
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
         </div>
         [// message text area]
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
         </div>
 
-        <button type="submit">Submit</button>
-        
+        <button type="submit" onSubmit={handleSubmit}>Submit</button>
+
       </form>
     </section>
   );
